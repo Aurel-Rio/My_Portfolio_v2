32dad95585a04809dab13458828bd774075cc4fc
debian-riozacki <debian-riozacki@riozacki-ms-7d18.home>
2023-03-10 12:31:04 +0100

    myporfolio_v2

--- src/components/Projet.js
+++ src/components/Projet.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../Projet.css";
 
 const Projet = ({ id, titre, techno, description, imageSrc, link }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+
   const settings = {
     dots: true,
     infinite: true,
@@ -23,6 +26,16 @@ const Projet = ({ id, titre, techno, description, imageSrc, link }) => {
     ]
   };
 
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedImage("");
+  };
+
   return (
     <div className="projet">
       <h4 id="rea_titre">{titre}</h4>
@@ -31,12 +44,27 @@ const Projet = ({ id, titre, techno, description, imageSrc, link }) => {
       <div className="carousel-container">
         <Slider {...settings} className="slider">
           {imageSrc.map((imageSrc, index) => (
-            <img key={index} src={imageSrc} alt="Illustration du projet" />
+            <img
+              key={index}
+              src={imageSrc}
+              alt="Illustration du projet"
+              onClick={() => handleImageClick(imageSrc)}
+            />
           ))}
         </Slider>
-
       </div>
       <div>{link}</div>
+
+      {showModal && (
+        <div className="modal" onClick={handleCloseModal}>
+          <span className="close" onClick={handleCloseModal}>
+            &times;
+          </span>
+          <div className="modal-content">
+            <img src={selectedImage} alt="Modal" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
